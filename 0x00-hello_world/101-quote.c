2377ce422b67ef36f5da8060e2e48b33121418de
Mohamed Ali <midobahebrasouly@gmail.com>
2023-10-15 14:13:53 -0700

    ll

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char qu[]="and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	write(1,qu, strlen(qu));
+	
+	return(1);
+}
