01859f5b8d87bb4033081c5cb30ebe5d3a02d2f6
Mohamed Ali <midobahebrasouly@gmail.com>
2023-10-23 18:55:38 -0700

    noo

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main (void)
+{
+        int digit = 0 , digit2 ;
+        while (digit <= 9)
+        {
+		digit2 = 0;
+		while (digit2 <= 9)
+		{
+			if(digit < digit2 && digit != digit2)
+			{
+				putchar(digit + 48);
+				putchar(digit +48);
+				if (digit + digit2 != 17 )
+				{
+					putchar(',');
+                       			putchar(' ');
+				}
+			}
+			digit2++;
+		}
+                digit++;
+        }
+        putchar('\n');
+        return (0);
+}
