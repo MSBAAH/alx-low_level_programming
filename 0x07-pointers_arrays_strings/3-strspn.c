7d03679dc73a8205622e04f764535615e92d0aa2
Mohamed Ali <midobahebrasouly@gmail.com>
2023-10-24 10:12:59 -0700

    not

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,11 +1,19 @@
-#include <main.h>
+#include "main.h"
+
+/**
+ * _strspn - check the code
+ * @s : h
+ * @accept : h
+ * Return: Always 0.
+ */
 
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned  int itr , jtr;
-	for ()
+	unsigned  int itr,  jtr;
+
+	for (itr = 0; s[itr] != '\0'; itr++)
 	{
-		for ()
+		for (jtr = 0; accept[jtr] != s[itr]; jtr++)
 		{
 			if (accept[jtr] == '\0')
 			{
