4a354aea3f9f3b8b6bb3bee3f45507f87c1225e9
Mohamed Ali <midobahebrasouly@gmail.com>
2023-10-24 09:30:18 -0700

    not yet

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,9 +1,16 @@
-#include <main.h>
+#include "main.h"
+/**
+ * _strchr - check the code
+ *@s : h
+ * @c : h
+ * Return: Always 0.
+ */
 
 char *_strchr(char *s, char c)
 {
 	int itr;
-	for(itr = 0; s[itr]>='\0'; itr++ )
+
+	for (itr = 0; s[itr] >= '\0'; itr++)
 	{
 		if (s[itr] == c)
 		{
