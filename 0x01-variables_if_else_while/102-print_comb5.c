1aba71ef6b79c79c5120d745d6ba7db931ec3b44
mdm <midobahebrasouly@gmail.com>
2024-07-09 05:22:35 -0700

    hh

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -5,31 +5,32 @@
  * Return: Always 0 (Success)
  */
 
-int main (void)
+int main(void)
 {
-        int digit = 0 , digit2 ;
-        while (digit <= 99)
-        {
-                digit2 = digit;
-                while (digit2 <= 99)
-                {
-                        if(digit < digit2 && digit != digit2)
-                        {
-                                putchar((digit / 10) + 48);
-                                putchar((digit % 10 ) +48);
+	int digit = 0, digit2;
+
+	while (digit <= 99)
+	{
+		digit2 = digit;
+		while (digit2 <= 99)
+		{
+			if (digit < digit2 && digit != digit2)
+			{
+				putchar((digit / 10) + 48);
+				putchar((digit % 10) + 48);
 				putchar(' ');
 				putchar((digit2 / 10) + 48);
-                                putchar((digit2 % 10 ) +48);
-                                if (digit != 98 || digit2 != 99 )
-                                {
-                                        putchar(',');
-                                        putchar(' ');
-                                }
-                        }
-                        digit2++;
-                }
-                digit++;
-        }
-        putchar('\n');
-        return (0);
+				putchar((digit2 % 10) + 48);
+				if (digit != 98 || digit2 != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			digit2++;
+		}
+		digit++;
+	}
+	putchar('\n');
+	return (0);
 }
