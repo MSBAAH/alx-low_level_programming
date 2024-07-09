b0666c13cb3ab0993599214a42fec00da9ccb15c
mdm <midobahebrasouly@gmail.com>
2024-07-09 09:24:11 -0700

    f

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,15 +5,15 @@
  *0s2: string 2
  *Return: 1 if true, 0 if false
 */
-int strcmp(char *s1, char *s2)
+int _strcmp(char *s1, char *s2)
 {
 	int equal = 0;
 	
 	while (*s1)
 	{
-		if (*s1 = *s2)
+		if (*s1 != *s2)
 		{
-			equal = ((int)*s1 -48) -((int)*s2 -48)
+			equal = ((int)*s1 - 48) -((int)*s2 - 48);
 			break;
 
 		}
