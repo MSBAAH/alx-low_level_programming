4df0cbbb5d22dd94312cb062c6f317038957fcba
mdm <midobahebrasouly@gmail.com>
2024-07-09 09:14:52 -0700

    f

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,20 +1,26 @@
 #include "main.h"
 
-/**stenopy A function that copies a string.
- * *@dest: pointer to destination input buffer Barce pointer to source input buffer 80: bytes of Barc
- * Return: dest
-*/  
-char *_strncpy (char *dest, char *src, int n)
+/**
+ * _strncpy-A function that copies a string.
+ *@dest: pointer to destination input buffer Barce pointer
+ * to source input buffer 80: bytes of Barc
+ *@n: pointer to destination input buffer Barce pointer
+ *@src: int
+ *Return: dest
+*/
+char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
-	/*iterate through src array
-	 * *where if there is no null byte among the first n bytes of source
-	 * *the string placed in dest will not be *hull terminated*/
-	for (i=0; i < n && src[i] != '\0'; i++)
+	/*
+	 * iterate through src array
+	 * where if there is no null byte among the first n bytes of source
+	 * the string placed in dest will not be *hull terminated
+	 */
+	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[i] = src[i];
 	while (i < n)
 	{
-		dest [i] = '\0';
+		dest[i] = '\0';
 		i++;
 	}
 	return (dest);
