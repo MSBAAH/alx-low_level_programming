534925f6a4fc822807ae71db0b3e90a03715367e
mdm <midobahebrasouly@gmail.com>
2024-07-09 03:31:39 -0700

    h

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,14 +10,14 @@ void print diagonal(int n)
 {
 	int postn, space;
 
-	if (n <<= 0)
+	if (n <= 0)
 		_putchar('\n');
 	else
 	{
 		for (postn = 1; postn <= n; postn++)
 		{
 			for (space = 1; space <= postn; space++)
-				_putchar();
+				_putchar(' ');
 			_putchar (92); /*is equal to '/' char*/
 			_putchar('\n');
 		}
