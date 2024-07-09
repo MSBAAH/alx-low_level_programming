03f8201b499755b713542a6c23338c9b92fad41d
mdm <midobahebrasouly@gmail.com>
2024-07-09 05:13:26 -0700

    hi

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -5,18 +5,19 @@
  * Return: Always 0 (Success)
  */
 
-int main (void)
+int main(void)
 {
-        char ch = 'a';
-        while (ch <= 'z')
-        {
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
 		if (ch == 'e' || ch == 'q')
 		{
 			ch++;
 		}
-                putchar(ch);
-                ch++;
-        }
-        putchar('\n');
-        return (0);
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
 }
