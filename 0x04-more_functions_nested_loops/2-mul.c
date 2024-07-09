7eda291eb4adec915132f9e0fe30cb1bf19a0fbd
mdm <midobahebrasouly@gmail.com>
2024-07-09 02:32:48 -0700

    h

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - check if c is upper
+ * @a: input for alphabet
+ * @b: input for alphabet
+ *
+ * Return: 1 if its uppercase, 0 if not
+*/
+int mul(int a, int b)
+{
+	if (c >= 48 && c <= 57)
+		return (a * b);
+}
