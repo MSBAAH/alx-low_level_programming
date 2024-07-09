9ea8d4aa98db65a1e09b5326ee16593fe0b8b1ef
mdm <midobahebrasouly@gmail.com>
2024-07-09 06:09:14 -0700

    f

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_rev - prints reversed string, followed by a Day 3
+ *@s: pointer to the string to print
+ * Return: vood
+*/
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+
+		i++;
+
+	while (i--)
+
+	{
+
+		_putchar (s[i]);
+
+	}
+
+	_putchar ('\n');
+}
