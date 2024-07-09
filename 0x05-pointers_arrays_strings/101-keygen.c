47276bea577d1b8f4ba76a6135812d25befe91aa
mdm <midobahebrasouly@gmail.com>
2024-07-09 07:13:19 -0700

    h

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ *main-generates candom passwords for 101-crackne * Returns zeza
+ *Return: int
+*/
+int main(void)
+{
+	int sum;
+	char c;
+
+	srand(time(NULL));
+	while (sum <= 2645)
+	{
+		c = rand() % 128;
+		sum += c;
+		putchar(c);
+	}
+	putchar (2772 - sum);
+	return (0);
+}
