c209c295e4b9a113b13b183d59128c58f47d9313
mdm <midobahebrasouly@gmail.com>
2024-07-09 08:21:21 -0700

    hi

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,12 +9,13 @@
 char *_strcat(char *dest, char *src)
 {
 	int c, c2;
+
 	C = 0;     /*find the size of dest array*/
-	while (dest [c])
+	while (dest[c])
 		c++;
 	/*iterate through each are array value without the null byte*/
 	for (c2 = 0; src[c2]; c2++)
 		/*append src[c2] to dest[c] while overwritting the null byte in dest*/
-		dest [C++] = src[c2];
+		dest[C++] = src[c2];
 	return (dest);
 }
